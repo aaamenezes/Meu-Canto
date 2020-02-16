c53a30b82693bc29b935f7e8b8679589dfaa29e6
aaamenezes <57503927+aaamenezes@users.noreply.github.com>
2020-02-16 19:48:36 -0300

    Formulário da página de anúncio
    Abas "enviar mensagem", "agendar visita" e "fazer proposta" funcionando.
    
    Falta apenas corrigir o tamanho do calendário que fica dentro da aba Agendar visita.

--- anuncio.js
+++ anuncio.js
@@ -1,17 +1,64 @@
 // Mudar 'abas' do formulário no lado direito do site
 
-function trocaAba(abaClicada, abaAnterior) {
+    // Definir variáveis
+let enviarMensagem, agendarVisita, fazerProposta
+let mensagem, visita, proposta
+
+    // Colocar cada aba em uma variável
+enviarMensagem = document.getElementById('enviarMensagem')
+agendarVisita = document.getElementById('agendarVisita')
+fazerProposta = document.getElementById('fazerProposta')
+
+mensagem = document.getElementById('mensagem')
+visita = document.getElementById('visita')
+proposta = document.getElementById('proposta')
+
+    // Bloquear direcionamento dos links
+document.getElementById("enviar").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("agendar").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("fazer").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+    // Remover e adicionar a borda as respectivas abas, e também ocultar e exibir o conteúdo respectivo de cada aba
+enviarMensagem.addEventListener("click", function() {
     
-    let container
-    container = document.getElementById('abas')
+    enviarMensagem.classList.add('selecionada')
+    mensagem.style.display = 'block'
 
-    //
+    agendarVisita.classList.remove('selecionada')
+    visita.style.display = 'none'
 
-}
-//clique numa aba
+    fazerProposta.classList.remove('selecionada')
+    proposta.style.display = 'none'
+})
 
-//adiciona borda inferior da aba anterior
-//oculta conteúdo da aba anterior
+agendarVisita.addEventListener("click", function() {
+    
+    enviarMensagem.classList.remove('selecionada')
+    mensagem.style.display = 'none'
 
-//remove borda inferior da aba clicada
-// exibe conteudo da aba clicada
+    agendarVisita.classList.add('selecionada')
+    visita.style.display = 'block'
+
+    fazerProposta.classList.remove('selecionada')
+    proposta.style.display = 'none'
+})
+
+fazerProposta.addEventListener("click", function() {
+    
+    enviarMensagem.classList.remove('selecionada')
+    mensagem.style.display = 'none'
+
+    agendarVisita.classList.remove('selecionada')
+    visita.style.display = 'none'
+
+    fazerProposta.classList.add('selecionada')
+    proposta.style.display = 'block'
+})
--- script.js
+++ script.js
@@ -95,30 +95,6 @@ function selectMudaInput() {
 
 }
 
-// Mês e ano atual calendário agendar visitas
-
-function mesAnoAtual() {
-    let mes = []
-    mes[0] = "Janeiro"
-    mes[1] = "Fevereiro"
-    mes[2] = "Março"
-    mes[3] = "Abril"
-    mes[4] = "Maio"
-    mes[5] = "Junho"
-    mes[6] = "Julho"
-    mes[7] = "Agosto"
-    mes[8] = "Setembro"
-    mes[9] = "Outubro"
-    mes[10] = "Novembro"
-    mes[11] = "Dezembro"
-
-    let ano = new Date().getFullYear()
-    
-    document.getElementById('mesAnoAtual').innerHTML = mes[new Date().getMonth()] + " de " + ano
-}
-
-mesAnoAtual()
-
 // Colorir botão de radio sim ou nao dos formularios
 
 // function colorirRadio() {
