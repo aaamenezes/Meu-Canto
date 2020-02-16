04e2370606ab484651b79b58411cd9c042170dfc
aaamenezes <57503927+aaamenezes@users.noreply.github.com>
2020-02-16 14:46:47 -0300

    Alteração no formulário da página de anúncio
    Adicionei as abas "enviar mensagem", "agendar visita" e "fazer proposta".
    
    Agora falta usar o JS pra mudar o estilo de borda delas e mudar o conteúdo do form quando clicar nelas

---
+++ anuncio.js
@@ -0,0 +1,17 @@
+// Mudar 'abas' do formulário no lado direito do site
+
+function trocaAba(abaClicada, abaAnterior) {
+    
+    let container
+    container = document.getElementById('abas')
+
+    //
+
+}
+//clique numa aba
+
+//adiciona borda inferior da aba anterior
+//oculta conteúdo da aba anterior
+
+//remove borda inferior da aba clicada
+// exibe conteudo da aba clicada
